fd21ce09ae015987ae6c4662771a264021e25f58
thi131190 <thi131190@gmail.com>
2019-10-01 17:42:11 +0700

    complete javascript code and add some styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,25 @@
-const exchangeRates = {
-  usd: {
-    eur: 0.91,
-    aud: 1.48,
-    krw: 1203.0,
-    vnd: 23200.7
-  },
-  eur: {
-    usd: 1.09,
-    aud: 1.62,
-    krw: 1316.21,
-    vnd: 25383.96
-  },
-  aud: {
-    usd: 0.68,
-    eur: 0.62,
-    krw: 813.58,
-    vnd: 15690.63
-  },
-  krw: {
-    usd: 0.00083,
-    aud: 0.0012,
-    eur: 0.00076,
-    vnd: 19.29
-  },
-  vnd: {
-    krw: 0.052,
-    usd: 0.000043,
-    aud: 0.000064,
-    eur: 0.000039
-  }
-};
+async function getConvertedValue(conversion, amount) {
+	const apiKey = '85f509d411c3ff2bc182';
+	const api = `https://free.currencyconverterapi.com/api/v6/convert?q=${conversion}&compact=y&apiKey=${apiKey}`;
+	const json = await fetch(api);
+	const result = await json.json();
+	console.log(result[conversion].val);
+	console.log(amount);
+	return amount * result[conversion].val;
+}
 
 function formatCurrency(type, value) {
-  const formatter = new Intl.NumberFormat(type, {
-    currency: type,
-    style: "currency"
-  });
-  return formatter.format(value);
+	const formatter = new Intl.NumberFormat(type, {
+		currency: type,
+		style: 'currency',
+	});
+	return formatter.format(value);
 }
 
-function convertCurrency(amount, fromCurrency, toCurrency) {
-  const convertedValue = (
-    amount * exchangeRates[fromCurrency][toCurrency]
-  ).toFixed(2);
-  return formatCurrency(toCurrency, convertedValue);
+async function convertCurrency() {
+	const fromCurrency = document.getElementById('from-currency').value;
+	const toCurrency = document.getElementById('to-currency').value;
+	const amount = document.getElementsByTagName('input')[0].value;
+	const convertedValue = await getConvertedValue(`${fromCurrency}_${toCurrency}`, amount);
+	document.getElementById('result').innerHTML = formatCurrency(toCurrency, convertedValue);
 }
-
-const fromCurrency = prompt("Which type of currency do you have? ");
-const amount = prompt("How much do you have?");
-const toCurrency = prompt("Which type of currency do you want to convert to? ");
-
-console.log(convertCurrency(amount, fromCurrency, toCurrency));
